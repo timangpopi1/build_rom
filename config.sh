6789a2956c7afcb0e042e8ce4c809ecb5ef23fdb
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-04-22 07:15:31 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-GITHUB_USER=JonasCardoso
-GITHUB_EMAIL=jonascard60@gmail.com
+GITHUB_USER=fadlyas07
+GITHUB_EMAIL=fadlyardhians@gmail.com
 
-KBUILD_BUILD_USER="jonas"
-KBUILD_BUILD_HOST="floppy"
+KBUILD_BUILD_USER="fadlyardhians"
+KBUILD_BUILD_HOST="buildbot"
 
-ROM="PixelExperience"
-manifest_url="https://github.com/PixelExperience/manifest"
-export rom_vendor_name="aosp" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
-branch="pie"
+ROM="ArrowOS"
+manifest_url="https://github.com/ArrowOS/android_manifest"
+export rom_vendor_name="arrow" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
+branch="arrow-9.x"
 
-release_repo="JonasCardoso/rom_build"
+release_repo="timangpopi1/new_rom"
 
-timezone="America/Sao_Paulo"
+timezone="Asia/jakarta"
