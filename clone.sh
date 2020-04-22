1cf7c8b27a9e61fadb7ad2978f050c5680beb82d
timangpopi1 <57018840+timangpopi1@users.noreply.github.com>
2020-04-22 08:03:24 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 mkdir .repo/local_manifests
-curl --create-dirs -L -o .repo/local_manifests/xiaomi_msm8996_default.xml -O -L https://raw.github.com/JonasCardoso/android_.repo_local_manifest/pe-pie/xiaomi_msm8996_default.xml >/dev/null  2>&1
+git clone https://github.com/timangpopi1/android-device-xiaomi-rolex -b master device/xiaomi/rolex/
+git clone https://github.com/Nick89786/android_vendor_xiaomi -b lineage-16.0 vendor/xiaomi/
+git clone https://github.com/fadlyas07/kernel_xiaomi_msm8917_sd425 -b master kernel/xiaomi/rolex/
